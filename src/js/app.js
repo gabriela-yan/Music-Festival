a9e7c2152e84bf234cb6d859ee0b6c53987d362d
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2021-02-16 17:17:53 -0600

    scrollIntoView

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log('Listo');
+document.addEventListener('DOMContentLoaded', function() {
+    scrollNav();
+});
+
+function scrollNav() {
+    const links = document.querySelectorAll('.navigation-main a');
+    links.forEach( function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            // console.log(event.target.attributes.href.value);
+
+            const section = document.querySelector(event.target.attributes.href.value);
+            section.scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
