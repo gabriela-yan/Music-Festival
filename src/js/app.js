a8ecb7f1e3df620b92c967963c0e00b8d973eaf4
Gabriela Yan <gabriela.maldonado.yan@gmail.com>
2021-02-16 18:53:49 -0600

    Intersection Observer

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     scrollNav();
+    fixedNavigation();
 });
+
+function fixedNavigation() {
+    const bar = document.querySelector('.header');
+    // Register the intersection observer
+    const observer = new IntersectionObserver( function(entries) {
+        // console.log(entries[0]);
+        if(entries[0].isIntersecting){
+            bar.classList.remove('fixed');
+        } else {
+            bar.classList.add('fixed');
+        }
+    })
+    // Element to observe
+    observer.observe(document.querySelector('.video'))
+}
 
 function scrollNav() {
     const links = document.querySelectorAll('.navigation-main a');
